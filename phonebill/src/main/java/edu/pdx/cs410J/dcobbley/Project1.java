d610123f0b5b4d1d92af4a3a62d8983b8b0ecc77
David <dcobbley@pdx.edu>
2015-07-08 14:54:22 -0700

    adding final touches

--- phonebill/src/main/java/edu/pdx/cs410J/dcobbley/Project1.java
+++ phonebill/src/main/java/edu/pdx/cs410J/dcobbley/Project1.java
@@ -27,20 +27,20 @@ public class Project1 {
   /**
    * Main will be called when the program is run, it parses the commands given by the user and calls the appropriate functionality.
    * @param args contains all the command line arguments passed into the program
-   *
+   * @throws IllegalArgumentException Exception if there is either not enough arguments or the wrong arguments
    */
   public static void main(String[] args) {
     Class c = AbstractPhoneBill.class;  // Refer to one of Dave's classes so that we can be sure it is on the classpath
       try {
           if (args.length == 0) //Check if there are no arguments
-              throw new Exception("Missing command line arguments");
+              throw new IllegalArgumentException("Missing command line arguments");
           if(args.length == 1 && args[0].contains("-README")){
               Readme();
               System.exit(0);
           }
           else if (args.length < 6) //Check if there are not enough arguments to be a complete phonecall
-              throw new Exception("Not enough args");      }
-      catch(Exception ex)
+              throw new IllegalArgumentException("Not enough args");      }
+      catch(IllegalArgumentException ex)
       {
         System.out.println(ex.getMessage());
         System.out.println("Usage: java edu.pdx.cs410J.<login-id>.Project1 [options] <args>\n" +
@@ -80,15 +80,15 @@ public class Project1 {
     endTime += args[6];
 
     if(startTime.contains("\"")||endTime.contains("\""))
-        throw new Exception("Date and time cannot contain quotes ");
+        throw new IllegalArgumentException("Date and time cannot contain quotes ");
     if(!callerNumber.matches("\\d{3}-\\d{3}-\\d{4}$")||!calleeNumber.matches("\\d{3}-\\d{3}-\\d{4}$"))
-        throw new Exception("Phone numbers must contain exactly 10 digits plus two dashes");
+        throw new IllegalArgumentException("Phone numbers must contain exactly 10 digits plus two dashes");
     if(!args[3].matches("(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])/((19|20)\\d\\d)")||!args[5].matches("(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])/((19|20)\\d\\d)"))
-        throw new Exception("Date format must follow mm/dd/yyyy");
+        throw new IllegalArgumentException("Date format must follow mm/dd/yyyy");
     if(!args[4].matches("([01]?[0-9]|2[0-3]):[0-5][0-9]")||!args[6].matches("([01]?[0-9]|2[0-3]):[0-5][0-9]"))
-        throw new Exception("Time format must follow mm:hh (24 hour time)");
+        throw new IllegalArgumentException("Time format must follow mm:hh (24 hour time)");
     }
-    catch (Exception ex){
+    catch (IllegalArgumentException ex){
         System.out.println(args[3]);
         System.out.println(ex.getMessage());
         System.exit(1);
@@ -112,21 +112,36 @@ public class Project1 {
                     System.out.println("Customer: " + myPhoneBill.getCustomer() + " " + myPhoneBill.getPhoneCalls());
                     break;
                 default:
-                    throw new Exception("Command Line Argument not found: \"" + args[x] +"\"");
+                    throw new IllegalArgumentException("Command Line Argument not found: \"" + args[x] +"\"");
             }
         }
     }
-    catch(Exception ex){
+    catch(IllegalArgumentException ex){
         System.out.println(ex.getMessage());
         System.exit(1);
     }
     System.exit(0);
   }
 
+    /**
+     * Readme function contains the readme of all useful information the user may need to know.
+     */
     private static void Readme() {
         System.out.println("README has been called");
         System.out.println("This program is a phonebill application which takes a very specific amount of arguments");
         System.out.println("You must provide a customer name, caller number, callee number, start time, and end time (mm/dd/yyyy mm:hh)");
+        System.out.println();
+        System.out.println("Usage: java edu.pdx.cs410J.<login-id>.Project1 [options] <args>\n" +
+                "   args are (in this order):\n" +
+                "       customer               Person whose phone bill we’re modeling\n" +
+                "       callerNumber           Phone number of caller\n" +
+                "       calleeNumber           Phone number of person who was called\n" +
+                "       startTime              Date and time call began (24-hour time)\n" +
+                "       endTime                Date and time call ended (24-hour time)\n" +
+                "   options are (options may appear in any order):\n" +
+                "       -print                 Prints a description of the new phone call\n" +
+                "       -README                Prints a README for this project and exits\n" +
+                "   Date and time should be in the format: mm/dd/yyyy hh:mm");
     }
 
 
